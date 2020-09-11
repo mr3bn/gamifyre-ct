8cf6db56327b11decf1a553f1e9e63535ee70876
ticknert <timothy.tickner@gmail.com>
2020-09-11 10:48:19 -0400

    Update git ignore and remove comments

--- functionapp/provision-activity/provision_activity.py
+++ functionapp/provision-activity/provision_activity.py
@@ -15,28 +15,6 @@ from __app__.shared.mongo_wrapper import get_client, insert_one
 
 def main(req: func.HttpRequest):
 
-    # SAMPLE req.get_json()
-    # {
-    #     "challenge_id": "5f56eca4616f41ee0202134d",
-    #     "name": "Daily Reading",
-    #     "record_rules": [
-    #         {
-    #             "name": "Read for 30 Minutes",
-    #             "type": "Type 1",
-    #             "required": true
-    #         }
-    #     ],
-    #     "repeat_rules": {
-    #         "repeats_allowed": true,
-    #         "repeat_frequency": "daily"
-    #     },
-    #     "reward_rules": {
-    #         "frequency_mutiplier": "5"
-    #     },
-    #     "completion_criteria": {
-    #         "team_completion": false
-    #     }
-    # }
     activity = req.get_json()
 
     client = get_client(os.environ[env_constants.MONGO_CONNECTION_STRING])
